504542211d140b125d919b1733b1d27e6f595408
Tayyab <tayyabali50055@gmail.com>
2025-07-09 17:13:23 +0500

    OAuth update

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
 
- function OAuth() {
+ /*function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -29,6 +29,36 @@ import { useNavigate } from "react-router-dom";
       navigate("/");
     } catch (error) {
       console.log("could not sign in google", error);
+    }
+  };
+  */
+
+  function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({ prompt: "select_account" }); // ✅ Force popup
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      alert(`Signed in as ${result.user.displayName}`);
+      const res = await fetch("https://backend-mernestate.vercel.app/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      console.log("could not sign in with google", error);
     }
   };
 
