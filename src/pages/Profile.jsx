f8e87e6f37d2448b0fac33dc8b6553343f234ef4
Tayyab <tayyabali50055@gmail.com>
2025-07-15 12:30:11 +0500

    profile update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -187,7 +187,11 @@ export default function Profile() {
   const handleShowListings = async () => {
     try {
       setShowListingsError(false);
-      const res = await fetch(`https://backend-mernestate-2.vercel.app/api/user/listings/${currentUser._id}`);
+      //const res = await fetch(`https://backend-mernestate-2.vercel.app/api/user/listings/${currentUser._id}`);
+      const res = await fetch(`https://backend-mernestate-2.vercel.app/api/user/listings/${currentUser._id}`, {
+      method: "GET",
+      credentials: "include", // 🔑 This sends the cookie with the request
+    });
       const data = await res.json();
       if (data.success === false) {
         setShowListingsError(true);
